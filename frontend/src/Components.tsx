ae8b2ce6cd0d0a74be875695989e0344b34cd2d8
Sean Humm <hummsean@pdx.edu>
2022-05-16 14:40:03 -0700

    weeks left is shown only when user submits data

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export type ExpectancyProps = {
@@ -6,9 +6,83 @@ export type ExpectancyProps = {
   getSearchClick: () => void,
 }
 
+export function Profile(props: ExpectancyProps) {
+  let {weeksLeft, getSearchClick} = props;
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const getExpectancy = () => {
+    setSubmitted(true)
+  }
+
+  const resetPage = () => {
+    setSubmitted(false)
+  }
+
+  return (
+    <div>
+      {/* If no data has been sent, show form */}
+      {submitted ? (
+          <>
+          <p>Weeks left is: {weeksLeft}</p>
+          <button onClick={resetPage}>
+            Reset
+          </button>
+          </>
+      ): (
+        <>
+        <SearchDataForm getExpectancy={getExpectancy} />
+        {/* Else, show expected output*/}
+        </>
+      )
+      }
+    </div>
+    
+  )
+}
+
+export const SearchDataForm = ({getExpectancy}) => {
+return (
+    <div className="formContainer">
+      <div className="form">
+          <div>
+            <label htmlFor="age">Age: </label> 
+            <input
+              type="text"
+              id="age"
+              required
+              name="age"
+              />
+            <br />
+
+            <label htmlFor="sex">Sex: </label>
+            <select name="sex" id="sex">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+            <br />
+
+            <label htmlFor="ethnicity">Ethnicity: </label>
+            <select name="eth" id="eth">
+              <option value="hispanic">Hispanic</option>
+              <option value="amerInAlaska">Non-Hispanic American Indian or Alaska Native</option>
+              <option value="asian">Non-Hispanic Asian</option>
+              <option value="black">Non-Hispanic Black</option>
+              <option value="white">Non-Hispanic White</option>
+            </select>
+            <br />
+            <button onClick={getExpectancy}> Search! </button>
+        </div>
+      </div> 
+    </div>
+  )
+}
+/*
 // Search Data to find life expectancy
 export function SearchData(props: ExpectancyProps) {
   let {weeksLeft, getSearchClick} = props;
+
+  const [submitted, setSubmitted] = useState(false);
 
   return (
     <div className="formContainer">
@@ -45,7 +119,7 @@ export function SearchData(props: ExpectancyProps) {
       </div> 
     </div>
   )
-}
+}*/
 
 export const NotFound = () => (
   <div>
