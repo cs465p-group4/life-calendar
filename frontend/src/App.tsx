ebc0a7725446dd39a710c5df8addd5b1e70be279
Sean Humm <hummsean@pdx.edu>
2022-05-16 13:02:03 -0700

    generate random life expectancy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,29 @@
-import React, { useEffect  } from 'react';
-import { SubmitData, NotFound, Header} from "./Components";
+import React, { useEffect, useState  } from 'react';
+import { SearchData, NotFound, Header} from "./Components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import initialState, { getLifeExpectancy } from './initialState';
 
 function App() {
+  let [currentProfile, setCurrentProfile] = useState(initialState.currentProfile);
 
   useEffect(() => {
     console.log("-- App rerenders --");
   });
+  
+  let getSearchClick = () => {
+    currentProfile = getLifeExpectancy();
+    setCurrentProfile(currentProfile)
+  };
+
+  let profile = <SearchData {...currentProfile} 
+    getSearchClick={getSearchClick} />
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Header />}>
-            <Route path="/" element={<SubmitData />} />
+            <Route path="/" element={profile} />
           </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -1,73 +1,49 @@
-import React, { useEffect, useState} from "react";
+import React from "react";
 import { Link, Outlet } from "react-router-dom";
-import { User } from "./services/UserService";
-import initialState from "./initialState";
 
-export const SubmitData = () => {
-
-  const [user, setUser] = useState(initialState);
-
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setUser({ ...user, [name]: value });
-  };
-
-  const saveData = () => {
-    User.create(user)
-  }
-
-  return (
-    <div>
-      <SetData handleInputChange={handleInputChange} saveData={saveData} user={user} />
-
-    </div>
-  )
+export type ExpectancyProps = {
+  weeksLeft: number,
+  getSearchClick: () => void,
 }
 
-export function SetData( { handleInputChange, saveData, user}) {
-
-  useEffect(() => {
-    console.log("Profile rerendered");
-  });
+export function SearchData(props: ExpectancyProps) {
+  let {weeksLeft, getSearchClick} = props;
 
   return (
-      <div className="formContainer">
-        <div className="form">
+    <div className="formContainer">
+      <div className="form">
           <div>
-            <form action={saveData}>
-              <label htmlFor="age">Age: </label> 
-              <input
-                type="text"
-                id="age"
-                required
-                value={user.age}
-                onChange={handleInputChange}
-                name="age"
-                />
-              <br />
+            <label htmlFor="age">Age: </label> 
+            <input
+              type="text"
+              id="age"
+              required
+              name="age"
+              />
+            <br />
 
-              <label htmlFor="sex">Sex: </label>
-              <select name="sex" id="sex">
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-              <br />
+            <label htmlFor="sex">Sex: </label>
+            <select name="sex" id="sex">
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+            <br />
 
-              <label htmlFor="ethnicity">Ethnicity: </label>
-              <select name="eth" id="eth">
-                <option value="hispanic">Hispanic</option>
-                <option value="amerInAlaska">Non-Hispanic American Indian or Alaska Native</option>
-                <option value="asian">Non-Hispanic Asian</option>
-                <option value="black">Non-Hispanic Black</option>
-                <option value="white">Non-Hispanic White</option>
-              </select>
-              <br />
-              <input type="submit" value="Submit"></input>
-            </form>
-          </div>
+            <label htmlFor="ethnicity">Ethnicity: </label>
+            <select name="eth" id="eth">
+              <option value="hispanic">Hispanic</option>
+              <option value="amerInAlaska">Non-Hispanic American Indian or Alaska Native</option>
+              <option value="asian">Non-Hispanic Asian</option>
+              <option value="black">Non-Hispanic Black</option>
+              <option value="white">Non-Hispanic White</option>
+            </select>
+            <br />
+            <p>Weeks left is {weeksLeft}!</p>
+            <button onClick={getSearchClick}> Search! </button>
         </div>
-      </div>
-  );
+      </div> 
+    </div>
+  )
 }
 
 export const NotFound = () => (
