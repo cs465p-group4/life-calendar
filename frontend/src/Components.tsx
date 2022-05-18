eaa318319605c52b59a0371cd3a5b7edcb6070e1
Sean Humm <hummsean@pdx.edu>
2022-05-18 15:13:09 -0700

    styling to show boxes representating weeks left

--- frontend/src/Components.tsx
+++ frontend/src/Components.tsx
@@ -23,8 +23,10 @@ export function Profile(props: ExpectancyProps) {
 
   const weeks: string[] = []
   for (let i = 0; i< weeksLeft; i++) {
-    weeks.push("x ")
+    weeks.push(" ")
   }
+
+  var renderBoxes = weeks.map(item =>  <div className="boxes"> {item} </div>)
 
   return (
     <div>
@@ -32,15 +34,22 @@ export function Profile(props: ExpectancyProps) {
       {submitted ? (
           <>
           <p>Weeks left is: {weeksLeft}</p>
-          <p>Visual Representation: {weeks}</p>
-          <button onClick={resetPage}>
-            Reset
-          </button>
+            <p>Visual Representation:</p>
+            <div className="display">
+              <div className="boxContainer">
+                {renderBoxes}
+              </div>
+              <div className="button">
+              <button onClick={resetPage}>
+                Reset
+              </button>
+            </div>
+          </div>
           </>
       ): (
         <>
-        <SearchDataForm getExpectancy={getExpectancy} />
         {/* Else, show expected output*/}
+        <SearchDataForm getExpectancy={getExpectancy} />
         </>
       )
       }
@@ -85,49 +94,6 @@ return (
     </div>
   )
 }
-/*
-// Search Data to find life expectancy
-export function SearchData(props: ExpectancyProps) {
-  let {weeksLeft, getSearchClick} = props;
-
-  const [submitted, setSubmitted] = useState(false);
-
-  return (
-    <div className="formContainer">
-      <div className="form">
-          <div>
-            <label htmlFor="age">Age: </label> 
-            <input
-              type="text"
-              id="age"
-              required
-              name="age"
-              />
-            <br />
-
-            <label htmlFor="sex">Sex: </label>
-            <select name="sex" id="sex">
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </select>
-            <br />
-
-            <label htmlFor="ethnicity">Ethnicity: </label>
-            <select name="eth" id="eth">
-              <option value="hispanic">Hispanic</option>
-              <option value="amerInAlaska">Non-Hispanic American Indian or Alaska Native</option>
-              <option value="asian">Non-Hispanic Asian</option>
-              <option value="black">Non-Hispanic Black</option>
-              <option value="white">Non-Hispanic White</option>
-            </select>
-            <br />
-            <p>Weeks left is {weeksLeft}!</p>
-            <button onClick={getSearchClick}> Search! </button>
-        </div>
-      </div> 
-    </div>
-  )
-}*/
 
 export const NotFound = () => (
   <div>
