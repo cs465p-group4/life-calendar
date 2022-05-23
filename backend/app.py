dd5f2523b2d8e198d2b5a02ef5005c6065eed2cb
Sean Humm <hummsean@pdx.edu>
2022-05-23 14:50:37 -0700

    organized code

--- backend/app.py
+++ backend/app.py
@@ -12,16 +12,16 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/comment", methods=['GET','POST'])
 @cross_origin(origin='localhost')
 def comments():
+    entries = {"name": [], "message": []}
     if(request.method=='POST'):
         data = request.get_json()
         model.insert(data['name'], data['message'])
-        print("insert happened")
     else:
-        entries = {"name": [], "message": []}
+        print("In GET")
         for row in model.select():
             entries["name"].append(row[0])
             entries["message"].append(row[1])
-    return redirect("/", entries=entries)
+    return entries
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
